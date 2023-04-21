06640ac7ba19117783a6bd2c2d90fb0b128b9bc9
romipuntel <r.puntel@hotmail.com>
2023-04-20 20:25:09 -0400

    cartManager

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import productRouter from './routes/product.routes.js'
-import cartRouter from './routes/cart.routes.js'
+import cartRouter from "./routes/cart.routes.js"
 import multer from 'multer'
 import { __dirname, __filename } from './path.js'
 
--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { CartManager } from "../CartManager";
+import { CartManager } from "../CartManager.js";
 
 const cartManager = new CartManager('./info.txt')
 
