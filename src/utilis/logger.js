94473afedb1e4d9d420719eadf7a7bdeb569be6f
romipuntel <r.puntel@hotmail.com>
2023-07-29 20:38:14 -0400

    logger

--- src/utilis/logger.js
+++ src/utilis/logger.js
@@ -45,11 +45,57 @@ const transports = [
     }),
 ];
 
-const logger = winston.createLogger({
-    level: level(),
-    levels,
-    format,
-    transports,
-});
 
-export default logger
+export const logger = winston.createLogger({
+    levels: levelOptions.levels,
+    transports: [
+        new winston.transports.Console({
+            level: "http",
+            format: winston.format.combine(
+                winston.format.colorize({ colors: levelOptions.colors }),
+                winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+                winston.format.simple()
+            )
+        }),
+        new winston.transports.File({
+            filename: "src/logs/fatal.log",
+            level: "fatal",
+            format: winston.format.combine(
+                winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+                winston.format.simple()
+            )
+        }),
+        new winston.transports.File({
+            filename: "src/logs/error.log",
+            level: "error",
+            format: winston.format.combine(
+                winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+                winston.format.simple()
+            )
+        }),
+        new winston.transports.File({
+            filename: "src/logs/warning.log",
+            level: "warning",
+            format: winston.format.combine(
+                winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+                winston.format.simple()
+            )
+        }),
+        new winston.transports.File({
+            filename: "src/logs/info.log",
+            level: "info",
+            format: winston.format.combine(
+                winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+                winston.format.simple()
+            )
+        }),
+        new winston.transports.File({
+            filename: "src/logs/http.log",
+            level: "http",
+            format: winston.format.combine(
+                winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+                winston.format.simple()
+            )
+        })
+    ]
+});
